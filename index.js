bb07c39c3f28c9e1b13ac8e5bbe20a7b566f2a20
GodMako <markus-mackan@hotmail.com>
2024-09-13 15:26:44 +0200

    HTML example + javascript on IF Statements

--- index.js
+++ index.js
@@ -197,19 +197,34 @@ myButton.onclick = function(){
 
 // let age = 0;
 
-// if (age >= 100) {
-//     console.log("You are TOO old to enter this site");
-// }
-// else if(age == 0) {
-//     console.log("You can`t enter. You were just born");
-// }
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+let age;
 
-// else if(age == 18) {
-//     console.log("You are old enough to enter this site");
-// }
-// else if(age <0) {
-//     console.log("Your age can`t be below 0");
-// } 
-// else {
-//     console.log("You must be enter 18+ to enter this site");
-// }
+mySubmit.onclick = function(){
+
+    age = myText.value;
+    age = Number(age);
+
+
+    if (age >= 100) {
+        resultElement.textContent = `You are TOO old to enter this site`;
+    }
+    else if(age == 0) {
+        resultElement.textContent = `You can't enter. You were just born`;
+    }
+    
+    else if(age >= 18) {
+        resultElement.textContent = `You are old enough to enter this site`
+    }
+    else if(age <0) {
+        resultElement.textContent = `Your age can't be below 0`;
+    }
+    else {
+        resultElement.textContent =`You must be enter 18+ to enter this site`
+    }
+}
+
+
+
