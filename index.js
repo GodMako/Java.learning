d1ab09c1fe2bde5a24a5f9f972bf06299f640f19
GodMako <markus-mackan@hotmail.com>
2024-09-14 01:51:11 +0200

    Number guessing game

--- index.js
+++ index.js
@@ -469,18 +469,52 @@ myButton.onclick = function(){
 
 // for loop = repeat some code a LIMITED amount of times
 
-for(let i = 10; i > 0; i-=2){
-    console.log(i);
+// for(let i = 10; i > 0; i-=2){
+//     console.log(i);
+// }
+
+// console.log("HAPPY NEW YEAR");
+
+// for(let i = 1; i <= 20; i++){
+
+//     if(i == 13){
+//         break;
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+let attempts = 0;
+let guess; 
+let running = true;
+
+while(running == true){
+
+    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`)
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number");
+    }
+    else if(guess < minNum || guess > maxNum){
+        window.alert("Please enter a number within the range");
+    } else {
+        attempts++;
+        if(guess < answer){
+            window.alert("TOO LOW! TRY AGAIN");
+        }
+        else if(guess > answer){
+            window.alert("TOO HIGH! TRY AGAIN");
+        }
+        else {
+            window.alert(`CORRECT! THE ANSWER WAS ${answer}. It took you ${attempts} attempts`);
+            running = false;
+        }
+    }
 }
 
-console.log("HAPPY NEW YEAR");
-
-for(let i = 1; i <= 20; i++){
-
-    if(i == 13){
-        break;
-    }
-    else{
-        console.log(i)
-    }
-}
