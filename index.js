7881453251695d9b2bfc33963a467e3fdf9427bf
GodMako <markus-mackan@hotmail.com>
2024-09-13 16:06:25 +0200

    Use Switch if there are too many ifs

--- index.js
+++ index.js
@@ -280,3 +280,58 @@ myButton.onclick = function(){
 // let purchaseAmount = 130;
 // let discount = purchaseAmount >= 100 ? 10 : 0 
 // console.log(`Your total is ${purchaseAmount - purchaseAmount *(discount/100)}`);
+
+SWITCH instead of if else - makes it less rep
+
+let day = 2;
+
+switch(day){
+    case 1: 
+        console.log("It is Monday");
+        break;
+    case 2: 
+        console.log("It is Tuesday");
+        break;
+    case 3: 
+        console.log("It is Wednesday");
+        break;
+    case 4: 
+        console.log("It is Thursday");
+        break;
+    case 5: 
+        console.log("It is Friday");
+        break;
+    case 6: 
+        console.log("It is Saturday");
+        break;
+    case 7: 
+        console.log("It is Sunday");
+        break;
+    default:
+        console.log(`${day} is not a day`);
+}
+
+let testScore = 32;
+let letterGrade;
+
+switch(true){
+    case testScore >= 90:
+        letterGrade = "A";
+        break;
+    case testScore >= 80:
+        letterGrade = "B";
+        break;
+    case testScore >= 70:
+        letterGrade = "C";
+        break;
+    case testScore >= 60:
+        letterGrade = "D";
+        break;
+    case testScore >= 50:
+        letterGrade = "Your grade is: E";
+        break;
+    default: 
+        letterGrade = "F";
+}
+
+console.log(letterGrade)
