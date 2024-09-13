9045f53e760fb23d02e8013621397514ce97ca4c
GodMako <markus-mackan@hotmail.com>
2024-09-14 01:26:51 +0200

    While loop - while and do

--- index.js
+++ index.js
@@ -431,11 +431,38 @@ myButton.onclick = function(){
 // != inequality operator
 // !== strict inequality operator
 
-const PI = 3.14;
+// const PI = 3.14;
 
-if(PI !== "3.14"){
-    console.log("That is NOT PI");
-}
-else {
-    console.log("That is PI");
+// if(PI !== "3.14"){
+//     console.log("That is NOT PI");
+// }
+// else {
+//     console.log("That is PI");
+// } 
+
+// while loop = repeat some code WHILE some condition is true
+
+let username;
+
+do{
+    username = window.prompt("Enter your name");
+} while(username === ""|| username === null)
+
+    console.log(`Hello ${username}`);
+
+let loggedIn = false;
+let username; 
+let password;
+
+while(!loggedIn){
+    username = window.prompt(`Enter your username`);
+    password = window.prompt(`Enter your password`);
+
+    if(username === "myUsername" && password === "myPassword"){
+        loggedIn = true;
+        console.log("You are logged in!");
+    }
+    else {
+        console.log("Invalid credentials! Please try again");
+    }
 } 
